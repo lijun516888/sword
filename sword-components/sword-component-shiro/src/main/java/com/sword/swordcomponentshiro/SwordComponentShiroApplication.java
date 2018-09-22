3a176ed4e8904349fd1c3e14ec1f16a41be109e3
lijun <576260496@qq.com>
2018-09-22 23:24:31 +0800

    初始化

---
+++ sword-components/sword-component-shiro/src/main/java/com/sword/swordcomponentshiro/SwordComponentShiroApplication.java
@@ -0,0 +1,12 @@
+package com.sword.swordcomponentshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordComponentShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordComponentShiroApplication.class, args);
+	}
+}
---
+++ sword-components/sword-component-shiro/src/test/java/com/sword/swordcomponentshiro/SwordComponentShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sword.swordcomponentshiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwordComponentShiroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

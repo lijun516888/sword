f8fcb265a3444df1f8fd5ebf277a4b06e7baa9e9
lijun <576260496@qq.com>
2018-09-23 09:20:38 +0800

    20180923-1

---
+++ sword-app/src/main/java/com/sword/app/Test.java
@@ -0,0 +1,10 @@
+package com.sword.app;
+
+import com.sword.core.utils.DateUtils;
+
+public class Test {
+
+    public static void main(String[] args) {
+        DateUtils.parse("");
+    }
+}
--- sword-core/src/main/java/com/sword/core/utils/DateUtils.java
+++ sword-core/src/main/java/com/sword/core/utils/DateUtils.java
@@ -1,4 +1,21 @@
 package com.sword.core.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
+
+    private static SimpleDateFormat sf = new SimpleDateFormat();
+
+    public static Date parse(String date) {
+        try {
+            sf.applyPattern("yyyy-MM-hh hh:mm:ss");
+            return sf.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }

cfc5521b1485c6755b7be171ff7b0045e61c8fd6
OK <1978665672@qq.com>
2018-11-01 22:59:16 +0800

    cloud

--- sword-cloud/sword-cloud-user/src/main/java/com/sword/cloud/user/UserCenter.java
+++ sword-cloud/sword-cloud-user/src/main/java/com/sword/cloud/user/UserCenter.java
@@ -3,9 +3,7 @@ package com.sword.cloud.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserCenter {

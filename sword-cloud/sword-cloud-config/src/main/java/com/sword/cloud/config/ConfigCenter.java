d3ad28f8bf82a6ac0d45d9c794a9f9f4413bcb36
lijun <576260496@qq.com>
2018-11-03 00:56:25 +0800

    初始化2

--- sword-cloud/sword-cloud-config/src/main/java/com/sword/cloud/config/ConfigCenter.java
+++ sword-cloud/sword-cloud-config/src/main/java/com/sword/cloud/config/ConfigCenter.java
@@ -2,11 +2,9 @@ package com.sword.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigCenter {
 

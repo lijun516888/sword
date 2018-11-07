4300906bf0aa3754e11dcc57a32d19b3c7d5eb2b
lijun <576260496@qq.com>
2018-11-07 23:53:22 +0800

    初始化2

--- sword-cloud/sword-cloud-auth/src/main/java/com/sword/cloud/auth/config/AuthorizationServerConfig.java
+++ sword-cloud/sword-cloud-auth/src/main/java/com/sword/cloud/auth/config/AuthorizationServerConfig.java
@@ -3,20 +3,23 @@ package com.sword.cloud.auth.config;
 import com.sword.cloud.auth.service.impl.RedisAuthorizationCodeServices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.token.TokenService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.AuthenticationKeyGenerator;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.util.UUID;
 
 @Configuration
@@ -31,6 +34,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Resource
     private RedisConnectionFactory redisConnectionFactory;
+
+    @Resource
+    private DataSource dataSource;
 
     /**
      * token储存器
@@ -48,7 +54,29 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.authenticationManager(this.authenticationManager);
         endpoints.tokenStore(tokenStore());
         endpoints.authorizationCodeServices(redisAuthorizationCodeServices);
-        super.configure(endpoints);
+        endpoints.tokenEnhancer(((oAuth2AccessToken, oAuth2Authentication) -> {
+            addLoginUserInfo(oAuth2AccessToken, oAuth2Authentication);
+            return oAuth2AccessToken;
+        }));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
+
+    public void addLoginUserInfo(OAuth2AccessToken accessToken, OAuth2Authentication auth2Authentication) {
+        if(accessToken instanceof DefaultOAuth2AccessToken) {
+            DefaultOAuth2AccessToken defaultOAuth2AccessToken = (DefaultOAuth2AccessToken) accessToken;
+            Authentication authentication = auth2Authentication.getUserAuthentication();
+            Object principal = auth2Authentication.getPrincipal();
+            // if(principal instanceof )
+        }
     }
 
     public static class RandomAuthenticationKeyGenerator implements AuthenticationKeyGenerator {
--- sword-cloud/sword-cloud-auth/src/main/java/com/sword/cloud/auth/service/impl/RedisAuthorizationCodeServices.java
+++ sword-cloud/sword-cloud-auth/src/main/java/com/sword/cloud/auth/service/impl/RedisAuthorizationCodeServices.java
@@ -1,4 +1,17 @@
 package com.sword.cloud.auth.service.impl;
 
-public class RedisAuthorizationCodeServices {
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.code.RandomValueAuthorizationCodeServices;
+
+public class RedisAuthorizationCodeServices extends RandomValueAuthorizationCodeServices {
+
+    @Override
+    protected void store(String s, OAuth2Authentication oAuth2Authentication) {
+
+    }
+
+    @Override
+    protected OAuth2Authentication remove(String s) {
+        return null;
+    }
 }
